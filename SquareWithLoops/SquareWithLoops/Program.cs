68445b5fca5d004a71dd52890d5327818f49d146
Jakooob <sokoljakub14@gmail.com>
2023-02-24 11:49:20 +0100

    Width and Height for SquareWithLoops

--- SquareWithLoops/SquareWithLoops/Program.cs
+++ SquareWithLoops/SquareWithLoops/Program.cs
@@ -6,11 +6,16 @@ namespace SquareWithLoops
     {
         public static void Main(string[] args)
         {
-            for (int i = 0; i < 4; i++)
+            Console.Write("Width: ");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Height: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 0; i < height; i++)
             {
-                for (int j = 0; j < 8; j++)
+                for (int j = 0; j < width; j++)
                 {
-                    Console.Write("*");
+                    Console.Write("*  ");
                 }
 
                 Console.WriteLine();
